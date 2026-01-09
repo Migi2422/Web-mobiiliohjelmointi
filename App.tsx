efb67d4a489f1afe3158ec0ce284ff25c5b32509
Migi2422 <milauril24@students.oamk.fi>
2026-01-09 15:06:43 +0200

    viikko1 sykemittari

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [age, setAge] = useState("");
+
+  const ageAsNumber = age ? Number(age) : 0;
+  const lower = age !== "" ? (220 - ageAsNumber) * 0.65 : 0;
+  const upper = age !== "" ? (220 - ageAsNumber) * 0.85 : 0;
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>sykemittari</Text>
+      <TextInput
+        keyboardType="number-pad"
+        placeholder="Ikä"
+        onChangeText={(text) => setAge(text)}
+        value={age}
+      />
+      <Text>Alaraja: {lower.toFixed(0)} bpm</Text>
+      <Text>Yläraja: {upper.toFixed(0)} bpm</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
